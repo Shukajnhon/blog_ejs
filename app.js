33036284ffbf857be9221bc9bbcdf08b9fa55cd1
Shukajnhon <87706728+Shukajnhon@users.noreply.github.com>
2022-10-12 14:10:29 +0700

    Blog-ejs v1

---
+++ app.js
@@ -0,0 +1,62 @@
+//
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+const homeStartingContent = "Hello guys!!"
+const aboutContent = "About!!"
+const contactContent = "Contact!!"
+
+const posts = [];
+
+// Home Page
+app.get('/', (req, res) => {
+    res.render("home", { startingContent: homeStartingContent })
+});
+
+// Contact Page
+app.get('/contact', (req, res) => {
+    res.render("contact", { contactContent: contactContent })
+});
+
+// About Page
+app.get('/about', (req, res) => {
+    res.render("about", { aboutContent: aboutContent })
+});
+
+// Compose Page
+app.get('/compose', (req, res) => {
+    res.render("compose")
+});
+
+app.post('/compose', (req, res) => {
+    const titleInput = req.body.postTitle
+    const textArea = req.body.postBody
+
+    const post = {
+        title: titleInput,
+        content: textArea
+    }
+
+    posts.push(post)
+
+    console.log(posts)
+    console.log(post)
+
+})
+
+
+
+
+
+
+
+app.listen(3000, (req, res) => {
+    console.log("Server is running on port 3000")
+});
